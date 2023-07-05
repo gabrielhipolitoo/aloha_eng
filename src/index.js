d8c58e95b77b8a004ee809b1361a40e1f26a0d8e
Gabriel Hipolito <c.gabrielhipolitoh@gmail.com>
2023-07-05 20:16:37 -0300

    fiz rem

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './assets/css/global.css'
-import './assets/css/animacaoScroll.css'
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
